272889832ceff1f0883e89701cb866bf484e0f70
Eyram Cobblah <ecobblah2@gmail.com>
2024-12-06 06:18:52 +0000

    Fix STATICFILES_DIR

--- azubieshop/settings.py
+++ azubieshop/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # HEROKU SETTINGS///////////////
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 django_heroku.settings(locals())
 # HEROKU SETTINGS///////////////
 
